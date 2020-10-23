7705e5657b378ecc45a6cf8b38ecc0b0f49b68a0
EugeneZakhno <zohan1234567@gmail.com>
2020-10-23 22:40:55 +0300

    init

---
+++ src/main/java/io/github/eugenezakhno/Main.java
@@ -0,0 +1,17 @@
+package io.github.eugenezakhno;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(Bot.getBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+}
